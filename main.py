59f0ed9db1bf6bbcc65823d0438bec903b379041
UddinNahim <nahim.211902019@gmail.com>
2025-12-13 04:41:26 +0600

    delete and put implement with raw sql

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException ,status
+from fastapi import FastAPI, HTTPException ,status , Response
 from pydantic import BaseModel, HttpUrl
 import psycopg2
 from psycopg2.extras import RealDictCursor
@@ -11,6 +11,10 @@ class Books(BaseModel):
     title: str
     author: str
     available: bool
+
+class UpdatedBooks(BaseModel):
+    title: str
+    author: str
 
 class Member(BaseModel):
     name: str
@@ -73,6 +77,67 @@ def get_book(id:int):
         detail=f"book with id:{id} was not found"
 
         )
-    return {"course_detail": book}
+    return {"book_detail": book} 
+
+@app.delete("/book/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_book(id: int):
+    # First delete related rows
+    cursor.execute("DELETE FROM borrow_records WHERE book_id = %s", (id,))
+    
+    # Now delete book
+    cursor.execute("DELETE FROM books WHERE id = %s RETURNING *", (id,))
+    dlt_book = cursor.fetchone()
+
+    conn.commit()
+
+    if not dlt_book:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Book with id {id} not found"
+        )
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/book/{id}")
+def update_book(id:int,books:UpdatedBooks):
+    cursor.execute("UPDATE books SET title = %s, author = %s where id = %s  RETURNING * " ,(books.title,books.author,id))
+    updated_book = cursor.fetchone()
+    conn.commit()
+    if  updated_book == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"books not found with this id = {id}"
+        )
+    return updated_book
+
+@app.put("/members/{id}")
+def update_member(id:int, member:Member):
+    cursor.execute("UPDATE members set name = %s , phone= %s WHERE id = %s RETURNING  *",(member.name,member.phone,id))
+    updated_member = cursor.fetchone()
+    conn.commit()
+    if updated_member == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"member not found with this id = {id}"
+        )
+    return updated_member
+
+
+
+
+
+@app.get("/members/{id}")
+def get_member(id:int):
+    cursor.execute("SELECT * FROM members where id = %s",(id,))
+    member = cursor.fetchone()
+    if not member:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Members not found with this id = {id}"
+        )
+    return {"member_detail": member}
+
+
 
     
